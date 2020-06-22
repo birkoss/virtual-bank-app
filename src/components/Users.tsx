4bb73e88875ff3cf76b2f2acb34faf2e9d4661ed
Mathieu Robichaud <admin@birkoss.com>
2020-06-22 08:21:19 -0400

    Cleaned the Users component to adapt data for children account

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -13,15 +13,20 @@ type Props = {
 
 export default function Users({ users, action }: Props) {
     const renderItem = ({ item }: { item: User }) => {
-        let description: string = item.email;
+        let description: string = item.is_children ? "" : item.email;
         if (item.accounts[0].balance !== undefined) {
             description += "\nBalance: " + item.accounts[0].balance + " $";
         }
 
+        let userFullname: string = item.firstname;
+        if (!item.is_children) {
+            userFullname += " " + item.lastname;
+        }
+
         return (
             <ListItem
-                title={`${item.firstname} ${item.lastname}`}
-                description={description}
+                title={userFullname}
+                description={description.trim()}
                 accessoryLeft={item.is_children ? UsersIcon : AdultIcon}
                 accessoryRight={() => action(item)}
             />
